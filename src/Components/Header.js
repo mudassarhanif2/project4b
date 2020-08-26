6edccec279cbe09aabb8e35bfb3743f2636f5c7a
Mudassar Hanif <mudassarhanif2@gmail.com>
2020-08-26 12:21:34 -1100

    updated a tag

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,17 +6,17 @@ import logo from './logo.png';
 export const Header = () => {
     return (
         <div className="container">
-            <img src={logo} className='img'></img>
+            <img src={logo} className='img' alt="animation video"></img>
             <ul className="links">
-                <li><a href=''>Home</a></li>
-                <li><a href=''>About Us</a></li>
-                <li><a href=''>Blog</a></li>
-                <li><a href='' >Services<select>
+                <li><button href=''>Home</button></li>
+                <li><button href=''>About Us</button></li>
+                <li><button href=''>Blog</button></li>
+                <li><button href='' >Services<select>
                     <option></option>
                     <option>2</option>
                     <option>3</option>
                     <option>4</option>
-                    </select></a></li>
+                    </select></button></li>
             </ul>
         </div>
     )
