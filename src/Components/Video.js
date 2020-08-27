007f6fde3eedbcfd56b4df026a1883f29d670051
Mudassar Hanif <mudassarhanif2@gmail.com>
2020-08-26 13:08:40 -1100

    updated a tag

--- src/Components/Video.js
+++ src/Components/Video.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import video from './junto.mp4';
-import './Video.css';
+// import './Video.css';
 
 export const Video = () => {
     return (
         <div className='main'>
             <div className='video'>
-                <video id='myVideo' autoPlay loop height='100%' width='100%'>
-                    <source src={video} type='video/mp4' />
-                </video>
+                <video src={video} id='myVideo' autoPlay loop height='100%' width='100%' />
             </div>
             <div className='button'><button href="">Let's make it happen</button></div>
         </div>
